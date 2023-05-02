89b50223a978ea16d5ac47a1c5e6a334422e78f6
Reverdan Almeida Sparinger <reverdan@gmail.com>
2023-05-02 20:14:40 -0300

    Triângulos, tipos
    Escaleno, equilátero e isósceles

---
+++ TiposTriangulos/src/modelo/Controle.java
@@ -0,0 +1,28 @@
+package modelo;
+
+public class Controle extends absPropriedades
+{
+
+    public Controle(String lado1, String lado2, String lado3)
+    {
+        super(lado1, lado2, lado3);
+    }
+
+    @Override
+    public void verificar()
+    {
+        this.mensagem = "";
+        Validacao validacao = new Validacao(lado1, lado2, lado3);
+        if (validacao.mensagem.equals(""))
+        {
+            Triangulos triangulos = new Triangulos(validacao.l1, 
+                    validacao.l2, validacao.l3);
+            this.resposta = triangulos.resposta;
+        }
+        else
+        {
+            this.mensagem = validacao.mensagem;
+        }
+    }
+    
+}
---
+++ TiposTriangulos/src/modelo/Validacao.java
@@ -0,0 +1,33 @@
+package modelo;
+
+public class Validacao extends absPropriedades
+{
+
+    public Validacao(String lado1, String lado2, String lado3)
+    {
+        super(lado1, lado2, lado3);
+    }
+
+    @Override
+    public void verificar()
+    {
+        this.mensagem = "";
+        try
+        {
+            this.l1 = Double.valueOf(lado1);
+            this.l2 = Double.valueOf(lado2);
+            this.l3 = Double.valueOf(lado3);
+            if (l1 + l2 <= l3 ||
+                    l2 + l3 <= l1 ||
+                    l1 + l3 <= l2)
+            {
+                this.mensagem = "Não é triângulo";
+            }
+        } 
+        catch (NumberFormatException e)
+        {
+            this.mensagem = "Erro de conversão";
+        }
+    }
+    
+}
